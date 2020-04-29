2b4c92ff43b5375db2fcfbc3a5bf5d939e7fda3e
Joshuabrotman <joshuabrotman@Gmail.com>
2020-04-29 09:02:26 -0500

    Final Commit before submission

--- TrashManagement/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ TrashManagement/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -92,25 +92,14 @@ namespace TrashManagement.Areas.Identity.Pages.Account
                     }
                     _logger.LogInformation("User created a new account with password.");
 
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = user.Id, code = code },
-                        protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    if(Input.Role == "Customer")
                     {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email });
+                        return RedirectToAction("Create", "Customers");
                     }
-                    else
+                    else if (Input.Role == "Employee")
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
-                        return LocalRedirect(returnUrl);
+                        return RedirectToAction("Create", "Employee");
                     }
                 }
                 foreach (var error in result.Errors)
---
+++ TrashManagement/Migrations/20200429130416_test123.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrashManagement.Migrations
+{
+    public partial class test123 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "12175e2c-3296-4b74-9251-0993f3d95b5b");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "81843d5d-eeb5-4122-a473-90d30dd694c6");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "5f7e2e8b-fab0-470d-aafc-c816ad175968", "eed9638d-e113-4dac-aca6-1f8b3ed7f938", "Customer", "CUSTOMER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "ee5b5e72-0e57-459a-94fd-0724a40fdd5e", "ef962efa-10ea-4b6d-93bd-5c838ba54f99", "Employee", "EMPLOYEE" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5f7e2e8b-fab0-470d-aafc-c816ad175968");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ee5b5e72-0e57-459a-94fd-0724a40fdd5e");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "12175e2c-3296-4b74-9251-0993f3d95b5b", "ac573cad-b22d-4573-9c33-db83fa1f2d19", "Customer", "CUSTOMER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "81843d5d-eeb5-4122-a473-90d30dd694c6", "d59bbea2-1557-4f3c-b02f-9c7ea1c106df", "Employee", "EMPLOYEE" });
+        }
+    }
+}
